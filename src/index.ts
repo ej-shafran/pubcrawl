50033994dbf27f89f8bf84850381356b70c55cbb
ej-shafran <ejshafran@hilma.tech>
2023-04-27 18:27:44 +0300

    Also export Subscription type

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { Publisher } from "./Publisher";
 export { Store } from "./Store";
 export { Network } from "./Network";
 export { TypedMap } from "./TypedMap";
+export type { Subscription } from "./Subscription";
