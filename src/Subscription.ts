67e33787525bb35b186774cdede0a85e0ad6b978
ej-shafran <ejshafran@hilma.tech>
2023-04-28 17:44:41 +0300

    Remove Subscription

--- src/Subscription.ts
+++
@@ -1,9 +0,0 @@
-/**
- * A subscription callback for a certain type `TData`.
- *
- * If `TData` is a function, this subscription function will accept its parameters as arguments.
- * Otherwise, the function will accept `TData`.
- **/
-export type Subscription<TData> = (
-  ...args: TData extends (...args: infer TArgs) => any ? TArgs : [TData]
-) => void;
